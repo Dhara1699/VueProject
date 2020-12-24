64f31851cf057a2fad901c43fda241f894415aa7
Dhara <dhara1.patel@bacancy.com>
2020-12-24 13:14:22 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="butterfly" src="./assets/butterfly.png">
+    <Home data = "My New Butterfly Page" msg="Hi, I'm Dhara"/>  
+  </div>
+</template>
+
+<script>
+ //import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+  //  HelloWorld,
+    Home 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir,'Times New Roman', serif, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #aeb4b9;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h1>Hello From New Trial Component By Me</h1>
+      <h2>{{data}}</h2>
+      <h3>{{msg}}</h3>
+    </div>
+</template>
+<script>
+export default {
+    name : 'Home',
+    props: {
+        data: String,
+        msg: String,
+        butterfly: Image
+    }
+}
+</script>
+
+<style>
+img {
+  border: 5px solid rgba(226, 157, 46, 0.616);
+}
+body {
+  background-color: rgb(26, 19, 17);
+}
+</style>
